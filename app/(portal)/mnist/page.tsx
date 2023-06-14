48bf9cc32340276cb03ffb4e0779bd18d8defb82
Henrique <hpeluzio@gmail.com>
2023-06-14 16:18:12 -0300

    Update mnist.css and page.tsx

--- app/(portal)/mnist/page.tsx
+++ app/(portal)/mnist/page.tsx
@@ -3,126 +3,57 @@
 import { useEffect, useRef, useState } from 'react';
 import * as ort from 'onnxruntime-web';
 import { Tensor } from 'onnxruntime-web';
-import Jimp from 'jimp';
-import './mnist.css';
 
 export default function Mnist() {
   const CANVAS_SIZE = 280;
-  const CANVAS_SCALE = 0.5;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const CANVAS_SCALE = 2;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
   const isDrawing = useRef(false);
-  const session = useRef<ort.InferenceSession>();
-  const [loading, setLoading] = useState<boolean>(false);
   const [predictions, setPredictions] = useState<any>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const debounceRef = useRef<any>(undefined);
-  // console.log('predictions', predictions);
 
   const initSession = async () => {
-    setLoading(true);
-
-    await ort.InferenceSession.create('./onnx_model.onnx', { executionProviders: ['webgl'], graphOptimizationLevel: 'all' });
-
     const canvas = canvasRef.current;
-    const context = canvas?.getContext('2d');
-    if (!canvas || !context) return;
+    const context = canvas!.getContext('2d');
 
-    // const image = new Image();
-    // image.src = './4.png';
-
-    // image.addEventListener('load', () => {
-    //   context.drawImage(image, 0, 0);
-    // });
-    // context.lineWidth = 28;
-    // context.lineJoin = 'round';
-    // context.font = '28px sans-serif';
-    // context.textAlign = 'center';
-    // context.textBaseline = 'middle';
-    // context.fillStyle = '#fff';
-    // context.fillText('Draw a number here...', CANVAS_SIZE / 2, CANVAS_SIZE / 2);
-    // clearCanvas();
-
-    // Set up event listeners
-    canvas.addEventListener('mousedown', startDrawing);
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mouseup', stopDrawing);
-    canvas.addEventListener('mouseout', stopDrawing);
-  };
-
-  const endSession = async () => {
-    const canvas = canvasRef.current;
-    const context = canvas?.getContext('2d');
-    if (!canvas || !context) return;
-
-    canvas.removeEventListener('mousedown', startDrawing);
-    canvas.removeEventListener('mousemove', draw);
-    canvas.removeEventListener('mouseup', stopDrawing);
-    canvas.removeEventListener('mouseout', stopDrawing);
-    setLoading(false);
+    contextRef.current = context;
+    context!.scale(2, 2);
+    context!.lineCap = 'round';
+    context!.strokeStyle = '#222222';
+    context!.lineWidth = 7;
+    context!.fillStyle = '#fff';
   };
 
   useEffect(() => {
     initSession();
 
-    setLoading(false);
-
-    // Cleanup code
-    return () => {
-      endSession();
-    };
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const startDrawing = (event: MouseEvent) => {
+  const startDrawing = ({ nativeEvent }: any) => {
     isDrawing.current = true;
-    draw(event);
+    const { offsetX: x, offsetY: y } = nativeEvent;
+    contextRef.current!.beginPath();
+    contextRef.current!.moveTo(x / CANVAS_SCALE, y / CANVAS_SCALE);
   };
 
-  const updatePredictions = async () => {
-    onnxruntime();
+  const stopDrawing = ({ nativeEvent }: any) => {
+    isDrawing.current = false;
+    contextRef.current!.closePath();
   };
 
-  const draw = (event: MouseEvent) => {
-    if (!isDrawing.current || !canvasRef.current) return;
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    const rect = canvas.getBoundingClientRect();
-    // const x = event.clientX - rect.left;
-    // const y = event.clientY - rect.top;
-    const CANVAS_SIZE = 280;
-    const CANVAS_SCALE = 0.5;
-    const x = event.offsetX / CANVAS_SCALE;
-    const y = event.offsetY / CANVAS_SCALE;
-    if (!context) return;
-
-    // Drawing code
-    // context.fillStyle = 'black';
-    // context.strokeStyle = 'black';
-    // context.lineWidth = 5;
-    // context.lineCap = 'round';
-    // context.fillRect(x, y, 20, 20);
-    // context.lineWidth = 28;
-    context.lineJoin = 'round';
-    context.font = '28px sans-serif';
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-
-    const radius = 15;
-    context.beginPath();
-    context.arc(x, y, radius, 0, 2 * Math.PI);
-    context.fill();
-    context.fillStyle = '#212121';
-    context.fillRect(x - 10, y - 10, 15, 15);
+  const draw = ({ nativeEvent }: any) => {
+    if (!isDrawing.current) return;
+    const { offsetX: x, offsetY: y } = nativeEvent;
+    contextRef.current!.lineTo(x / CANVAS_SCALE, y / CANVAS_SCALE);
+    contextRef.current!.stroke();
 
     clearTimeout(debounceRef.current);
 
     debounceRef.current = setTimeout(() => {
       updatePredictions();
-    }, 150);
-  };
-
-  const stopDrawing = () => {
-    isDrawing.current = false;
+    }, 100);
   };
 
   const clearCanvas = () => {
@@ -131,8 +62,8 @@ export default function Mnist() {
 
     if (!canvas || !context) return;
 
-    // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
+
     setPredictions([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   };
 
@@ -144,135 +75,70 @@ export default function Mnist() {
     link.click();
   };
 
-  const onnxruntime = async () => {
-    console.log('onnxruntime');
-    async function loadImageFromPath(pngImage: string, width: number = 280, height: number = 280): Promise<Jimp> {
-      // Use Jimp to load the image and resize it.
-      const imageData = await Jimp.read(pngImage).then((imageBuffer: Jimp) => {
-        return imageBuffer.resize(width, height);
-      });
-
-      return imageData;
-    }
-
-    function imageDataToTensor(image: Jimp, dims: number[]): Tensor {
-      // 1. Get buffer data from image and create R, G, and B arrays.
-      var imageBufferData = image.bitmap.data;
-      const [redArray, greenArray, blueArray] = new Array(new Array<number>(), new Array<number>(), new Array<number>());
-
-      // // 2. Loop through the image buffer and extract the R, G, and B channels
-      // for (let i = 0; i < imageBufferData.length; i += 4) {
-      //   redArray.push(imageBufferData[i]);
-      //   greenArray.push(imageBufferData[i + 1]);
-      //   blueArray.push(imageBufferData[i + 2]);
-      //   // skip data[i + 3] to filter out the alpha channel
-      // }
-
-      // // 3. Concatenate RGB to transpose [224, 224, 3] -> [3, 224, 224] to a number array
-      // const transposedData = redArray.concat(greenArray).concat(blueArray);
-
-      // // 4. convert to float32
-      // let i,
-      //   l = transposedData.length; // length, we need this for the loop
-      // // create the Float32Array size 3 * 224 * 224 for these dimensions output
-      const float32Data = new Float32Array(dims[1] * dims[2] * dims[3]);
-      // for (i = 0; i < l; i++) {
-      //   float32Data[i] = transposedData[i] / 255.0; // convert to float
-      // }
-      // 5. create the tensor object from onnxruntime-web.
-      const inputTensor = new Tensor('float32', float32Data, [313600]);
-      return inputTensor;
-    }
-
+  const updatePredictions = async () => {
     const canvas = canvasRef.current;
     const context = canvas!.getContext('2d');
-    // const pngImage = canvas!.toDataURL('image/png');
     const imgData = context!.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    // const imageDataJimp = await loadImageFromPath(pngImage);
-    // console.log('Jimp imageData', imageDataJimp);
-
-    const dims: number[] = [1, 4, 280, 280];
-    // const imageTensor = imageDataToTensor(imageDataJimp, dims);
     const imageTensor = new Tensor('float32', new Float32Array(imgData.data), [313600]);
-    // console.log('imageTensor ->', imageTensor);
 
-    async function runSqueezenetModel(preprocessedData: any): Promise<void> {
-      // Create session and set options. See the docs here for more options:
-      //https://onnxruntime.ai/docs/api/js/interfaces/InferenceSession.SessionOptions.html#graphOptimizationLevel
-      const session = await ort.InferenceSession.create('./onnx_model.onnx', { executionProviders: ['webgl'], graphOptimizationLevel: 'all' });
-      // console.log('Inference session created');
-      // Run inference and get results.
-      // var [results, inferenceTime] =  await runInference(session, preprocessedData);
-      // console.log('session', session);
+    const session = await ort.InferenceSession.create('./onnx_model.onnx', { executionProviders: ['webgl'], graphOptimizationLevel: 'all' });
+    const feeds: Record<string, ort.Tensor> = {};
+    feeds[session.inputNames[0]] = imageTensor;
 
-      // create feeds with the input name from model export and the preprocessed data.
-      const feeds: Record<string, ort.Tensor> = {};
-      feeds[session.inputNames[0]] = preprocessedData;
-
-      const outputData = await session.run(feeds);
-      // console.log('session.inputNames[0]', session.outputNames[0]);
-      // console.log('outputData', outputData[session.outputNames[0]].data);
-      setPredictions(Array.prototype.slice.call(outputData[session.outputNames[0]].data));
-      // console.log('settings predictions');
-    }
-
-    runSqueezenetModel(imageTensor);
-
-    // // Download PNG
-    // const dataUrl = await imageDataJimp.getBase64Async(Jimp.MIME_PNG);
-    // const link = document.createElement('a');
-    // link.href = dataUrl;
-    // link.download = 'image.png'; // Set the desired file name and extension
-
-    // // Simulate a click to trigger the download
-    // link.click();
-
-    // // Clean up by removing the temporary anchor element
-    // link.remove();
+    const outputData = await session.run(feeds);
+    setPredictions(Array.prototype.slice.call(outputData[session.outputNames[0]].data));
   };
-
-  const predictionBarHeight = (prediction: any) => {
-    // console.log('predictionBarHeight', prediction);
-    return `${prediction * 100}%`;
-  };
-
-  const backgroundColorBar = (prediction: any) => {
-    const max = Math.max(...predictions);
-    if (prediction === max) return `#00F0FF`;
-    return '#E0E0E0';
-  };
-
-  if (loading) return <div>Loading...</div>;
 
   return (
-    <main>
-      <div className="flex flex-col items-center">Mnist</div>
-      <div className="card elevation">
-        <canvas ref={canvasRef} className="canvas elevation" id="canvas" width="280" height="280" />
+    <div className="flex flex-col items-center">
+      <canvas
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseMove={draw}
+        className="mb-5 rounded bg-white"
+        width="280"
+        height="280"
+      />
 
-        <div className="button" id="clear-button" onClick={clearCanvas}>
+      <div className="flex flex-col">
+        <button
+          className={`
+          w-28 h-10 bg-neutral-400 dark:bg-neutral-600 
+          hover:bg-neutral-500 dark:hover:bg-neutral-500 
+          text-neutral-100 dark:text-neutral-200
+          rounded
+          mb-5
+        `}
+          onClick={clearCanvas}
+        >
           CLEAR
-        </div>
+        </button>
 
-        <div className="button" id="download-button">
-          <button onClick={handleDownload}>Download as PNG</button>
-        </div>
-
-        <div className="button" id="onnxruntime-button">
-          <button onClick={onnxruntime}>Predict once</button>
-        </div>
-
-        <div className="predictions">
-          {predictions.map((e: any, i: number) => (
-            <div key={i} className="prediction-col">
-              <div className="prediction-bar-container">
-                <div className="prediction-bar" style={{ height: predictionBarHeight(e), backgroundColor: backgroundColorBar(e) }}></div>
-              </div>
-              <div className="prediction-number">{i}</div>
-            </div>
-          ))}
-        </div>
+        <button
+          className={`
+          w-28 h-14 bg-neutral-400 dark:bg-neutral-600 
+          hover:bg-neutral-500 dark:hover:bg-neutral-500 
+          text-neutral-100 dark:text-neutral-200
+          rounded
+          mb-5
+        `}
+          onClick={handleDownload}
+        >
+          Download as PNG
+        </button>
       </div>
-    </main>
+
+      <div className="flex">
+        {predictions.map((e: any, i: number) => (
+          <div key={i} className="pt-0 pe-2">
+            <div className="flex flex-col-reverse h-36 w-4 bg-neutral-600 dark:bg-neutral-300">
+              <div style={{ height: `${e * 100}%` }} className={`${e === Math.max(...predictions) ? 'bg-green-600' : 'bg-green-300'}`}></div>
+            </div>
+            <div className="text-lg">{i}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
