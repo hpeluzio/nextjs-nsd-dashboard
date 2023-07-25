974be6cc37e48f5ab886cb29e5f6aa6d89a9834d
Henrique Peluzio <hpeluzio@gmail.com>
2023-07-25 11:19:33 -0300

    Update SideNav.tsx

--- app/components/template/SideNav.tsx
+++ app/components/template/SideNav.tsx
@@ -20,8 +20,8 @@ export default function SideNav() {
         {/* <NavItem url="/settings" text="Settings" icon={SettingsIcon} /> */}
         {/* <NavItem url="/notifications" text="Notifications" icon={Notifications} /> */}
         <NavItem url="/mnist" text="Mnist" icon={CalculatorIcon} />
-        <NavItem url="/cifar10" text="Cifar10 Mobile 74%" icon={CalculatorIcon} />
-        <NavItem url="/cifar10-api" text="Cifar10 API 99%" icon={CalculatorIcon} />
+        <NavItem url="/cifar10" text="Cifar10 Mobile" icon={CalculatorIcon} />
+        <NavItem url="/cifar10-api" text="Cifar10 API" icon={CalculatorIcon} />
         {/* <NavItem url="/bjj" text="BJJ" icon={CalculatorIcon} /> */}
         <NavItem url="/onnxruntime" text="Onnxruntime Web" icon={CalculatorIcon} />
         <NavItem url="/drawing" text="Drawing" icon={PencilIcon} />
